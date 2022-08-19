681a64155b91a1e2be747ddadad84a294887ffaf
rifatabrajowad <rajdiprifat2004@gmail.com>
2022-08-20 03:44:58 +0600

    nav - css

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -5,7 +5,7 @@ function Nav() {
         <nav className={classes.nav}>
             <ul>
                 <li>
-                    <a href="index.html" className="brand">
+                    <a href="index.html" className={classes.brand}>
                         <img src="./images/logo-bg.png" alt="Learn with Sumit Logo"/>
                         <h3>Learn with Sumit</h3>
                     </a>
