a7bb1826cf3162492d569b4454ca574c46091fe3
rifatabrajowad <rajdiprifat2004@gmail.com>
2022-08-20 03:49:32 +0600

    image-set

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,12 +1,13 @@
 import Account from "./Account";
 import classes from "../css/Nav.module.css";
+import logo from "../assets/images/logo-bg.png";
 function Nav() {
     return (
         <nav className={classes.nav}>
             <ul>
                 <li>
                     <a href="index.html" className={classes.brand}>
-                        <img src="./images/logo-bg.png" alt="Learn with Sumit Logo"/>
+                        <img src={logo} alt="Learn with Sumit Logo"/>
                         <h3>Learn with Sumit</h3>
                     </a>
                 </li>
