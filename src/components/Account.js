5c7aeed18802ed7d7528fc5e8e7311d3521c85b4
rifatabrajowad <rajdiprifat2004@gmail.com>
2022-08-20 03:52:13 +0600

    image-set

--- src/components/Account.js
+++ src/components/Account.js
@@ -1,7 +1,11 @@
 function Account() {
     return (
-        <div className="App">
-            <h1>Hello World</h1>
+        <div className="account">
+        <span className="material-icons-outlined" title="Account">
+          account_circle
+        </span>
+            <a href="signup.html">Signup</a>
+            <!-- <span class="material-icons-outlined" title="Logout"> logout </span> -->
         </div>
     );
 }
