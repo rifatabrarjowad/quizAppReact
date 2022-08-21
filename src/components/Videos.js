e4fa4424782277e177cb502fc45fa8553d93c4c2
rifatabrajowad <rajdiprifat2004@gmail.com>
2022-08-22 03:27:21 +0600

    video css update

---
+++ src/components/Videos.js
@@ -0,0 +1,13 @@
+import classes from '../css/video.module.css';
+import Video  from "./Video";
+function Videos() {
+
+
+    return (
+<div className={classes.videos}>
+    <Video />
+</div>
+    );
+}
+
+export default Videos;
