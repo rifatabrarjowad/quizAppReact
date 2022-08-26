634e3042a4e340c1fefead821b3bbf100c509826
rifatabrajowad <rajdiprifat2004@gmail.com>
2022-08-26 23:23:17 +0600

    sign up

---
+++ src/components/Pages/Signup.js
@@ -0,0 +1,12 @@
+// import image from '../assets/images/3.jpg';
+// import classes from '../css/video.module.css';
+
+function Signup() {
+
+
+    return (
+        <h1>Hello From Video </h1>
+    );
+}
+
+export default Signup;
